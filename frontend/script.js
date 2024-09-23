561dd03af5d0dfbffb6988a92eb1c08605a3999f
Mbekezeli Sithole <35656656+mbekesithole@users.noreply.github.com>
2024-09-23 10:45:39 +0100

    created viewer count JS function

--- frontend/script.js
+++ frontend/script.js
@@ -4,3 +4,26 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  getVisitCount();
+});
+
+const functionApi = "";
+
+const getVisitCount = () => {
+  let count = 30;
+  fetch(functionApi)
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      console.log("Website called function API!");
+      count = response.count;
+      document.getElementById("counter").innerText = count;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  return count;
+};
